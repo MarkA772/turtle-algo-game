639c22203d744788f9c289df99a01a1460218c60
Mark Alexander <markalexander@example.com>
2022-06-15 14:30:08 -0700

    Finished initial webpack and server setup for react app

---
+++ client/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+class App extends React.Component {
+
+  render() {
+    return <div>Hello from App.jsx!</div>
+  }
+}
+
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'client', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: './client/index.html'
+  })]
+};
