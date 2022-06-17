a487e48945cf2fd17c8002d21ff7df6eae5a183a
Mark Alexander <markalexander@example.com>
2022-06-17 09:43:33 -0700

    Make approach notes for turtle logic

--- client/components/Canvas.jsx
+++ client/components/Canvas.jsx
@@ -19,5 +19,33 @@ class Canvas extends React.Component {
   }
 }
 
+/**
+ * Turtle class to encapsulate turtle drawing logic. Canvas component should
+ *   create this class and pass in the canvas context for it to work with.
+ * 
+ * Canvas component will send events to the Turtle instance from react.
+ * Canvas component does not need to know what the turtle is doing, as it
+ *   will be drawing directly to the canvas and won't need react to be updated.
+ * 
+ * Turtle should draw slowly, so user can see the turtle as it draws the lines
+ *   and patterns.
+ * 
+ * To start with, we need functions for turning left/right at an angle, moving
+ *   forward x pixels, and back x pixels, and drawing a line.
+ * 
+ * We also need capability to call those functions in loops.
+ * We also need to keep in mind that when we parse these commands it will have
+ *   the loop count and the function names inside them with their own args.
+ * So this can be complicated if not set up properly. We need a flexible way
+ *   to call this funcionality in many different ways and different orders.
+ */
+class Turtle {
+  constructor() {
+    this.posx = 0;
+    this.posy = 0;
+    this.orientation = 0; // degrees orientation, 0 is upwards
+  }
+}
+
 
 export default Canvas;
