8d35732146c80b6757d1cc9165c2fb9e347dac96
Mark Alexander <markalexander@example.com>
2022-06-20 11:47:58 -0700

    Sidebar moves smoothly and scrolls without overflow

--- client/components/SideBar.jsx
+++ client/components/SideBar.jsx
@@ -2,12 +2,43 @@ import React from 'react';
 
 
 class SideBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.sidebarRef = React.createRef(null);
+    this.state = {
+      sidebarOpen: false,
+    }
+  }
+
+  toggleNav() {
+    if (this.state.sidebarOpen) {
+      this.setState({sidebarOpen: false});
+    } else {
+      this.setState({sidebarOpen: true});
+    }
+    // const sidebar = this.sidebarRef.current;
+    // console.log(sidebar.style.width)
+    // sidebar.style.width = '100px';
+  }
 
   render() {
     return (
-    <><div className='sidebar'>Test
+    <>
+    <div
+      className='sidebar'
+      style={{width: this.state.sidebarOpen ? '250px' : '0px'}}
+      ref={this.sidebarRef}
+    >
+      <div className='sidebar-inner'>
+        Test
+      </div>
     </div>
-    <button className='sidebar-button'>&gt;</button>
+    <button
+       className='sidebar-button'
+       onMouseDown={() => this.toggleNav()}
+    >
+      &gt;
+    </button>
     </>
     );
   }
